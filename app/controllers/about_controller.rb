c50dbe029153948aa357c33ffd00144f642a42fa
takeokunn <bararararatty@gmail.com>
2017-05-25 20:42:39 +0900

    最近のThink!のデザイン修正

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -5,7 +5,7 @@ class AboutController < ApplicationController
   before_action :set_instance_presenter, only: [:show, :more]
 
   def show
-    @status = Status.with_public_visibility.local.first
+    @status = Status.joins(:account).with_public_visibility.local.first
   end
 
   def more; end
