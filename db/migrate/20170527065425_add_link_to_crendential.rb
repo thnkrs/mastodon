ed8d2909b9f50137da06a23dc8467117d638680f
Satoshi SUZUKI <akn@s-satoshi.net>
2017-05-30 17:59:49 +0900

    change link column to nullable

--- db/migrate/20170527065425_add_link_to_crendential.rb
+++ db/migrate/20170527065425_add_link_to_crendential.rb
@@ -1,5 +1,5 @@
 class AddLinkToCrendential < ActiveRecord::Migration[5.0]
   def change
-    add_column :credentials, :link, :string, null: false
+    add_column :credentials, :link, :string, null: true
   end
 end
