9cc1493e942a22d40ab69e0ebf9f36346b1a7f91
Satoshi SUZUKI <akn@s-satoshi.net>
2017-06-14 01:37:46 +0900

    debug backup script

--- backup_db.sh
+++ backup_db.sh
@@ -7,5 +7,5 @@ TEMP_FILE=$(mktemp tmp.XXXXXXXXXX)
 S3_FILE="s3://$BUCKET_NAME/postgres/$TIMESTAMP.sql"
 
 docker-compose exec db pg_dumpall -l postgres -U postgres > $TEMP_FILE
-s3cmd put $TEMP_FILE $S3_FILE
+aws s3 cp $TEMP_FILE $S3_FILE
 rm "$TEMP_FILE"
